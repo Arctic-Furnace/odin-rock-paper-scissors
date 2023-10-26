f0ba447479c1b017e79c1150ec254c1072767c88
Arctic-Furnace <richardjameshaddon@gmail.com>
2023-10-26 11:53:59 +0100

    add function to play a round without player input

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -15,4 +15,22 @@ function getComputerChoice(){
     }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection){
+    if(playerSelection.toLowerCase() === "rock" && computerSelection === "Scissors" || playerSelection.toLowerCase() === "paper" && computerSelection === "Rock"  || playerSelection.toLowerCase() === "scissors" && computerSelection === "Paper")
+    {
+        return "You Win! " + playerSelection + " beats " + computerSelection;
+    }
+    else if(playerSelection.toLowerCase() === "rock" && computerSelection === "Paper" || playerSelection.toLowerCase() === "paper" && computerSelection === "Scissors"  || playerSelection.toLowerCase() === "scissors" && computerSelection === "Rock")
+    {
+        return "You Lose! " + computerSelection + " beats " + playerSelection;
+    }
+    else
+    {
+        return "Its a draw";
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
